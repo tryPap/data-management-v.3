dfa5f95a8ac8f3aba8ab1bd3b1a40290b006ad23
tryPap <t.papagohn@gmail.com>
2024-12-18 14:20:32 +0200

    title

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
   return (
     <div>
       <nav className="navbar">
-          <h1 className="headtitle">Data Management Pro</h1>
+          <h1 className="headtitle">Data Management v.3</h1>
           
       </nav>
     </div>
